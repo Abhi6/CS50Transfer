c0681386ff2e80bd5dd04b69bd0f7b5e8840055e
Abhi6 <abhinav.mara@gmail.com>
2022-11-26 21:09:44 -0500

    Spaced title and paragraph better, debugged unwanted display of paragraphs at start of opening webpage (basically created the transition in CSS via Javascript when necessary)

--- psets/pset8/homepage/pages/scriptsDir/resume.js
+++ psets/pset8/homepage/pages/scriptsDir/resume.js
@@ -2,20 +2,23 @@ let entryInformation = document.querySelectorAll(".entry-content");
 
 for (var i = 0; i < entryInformation.length; i = i + 1) {
     entryInformation[i].addEventListener("mouseover", function() {
+        // glide the title from the middle to the top
         let entryTitle = this.querySelector("h3");
         entryTitle.style.transition = "top 0.5s";
-        // entryTitle.style.transform = "translate(-50%, 0)";
-        entryTitle.style.top = "0%";
+        entryTitle.style.top = "10%";
+        // make the information appear
         let entryParagraph = this.querySelector("p");
+        entryParagraph.style.transition = "opacity 3s"; // Slowed down to sync with title movement
         entryParagraph.style.opacity = "1";
     });
     entryInformation[i].addEventListener("mouseout", function() {
+        // glide the title from the top to the middle
         let entryTitle = this.querySelector("h3");
         entryTitle.style.transition = "top 3s";
-        // entryTitle.style.transform = "translate(-50%, -50%)";
         entryTitle.style.top = "50%";
+        // make the information disappear
         let entryParagraph = this.querySelector("p");
-        entryParagraph.style.transition = "opacity 0.5s";
+        entryParagraph.style.transition = "opacity 0.5s"; // Disappear faster to sync with title movement
         entryParagraph.style.opacity = "0";
     });
 }
