96acc8826cf69ff5df2fd1791846382d64fa0566
Abhi6 <abhinav.mara@gmail.com>
2022-12-11 05:44:17 -0500

    animation changes added, TODO: make animations smooth and typography

--- psets/pset8/homepage/pages/scriptsDir/hobbies.js
+++ psets/pset8/homepage/pages/scriptsDir/hobbies.js
@@ -1,1 +1,35 @@
-console.log('hobbies testing')
+var volleyballCard = document.getElementById("volleyball-card");
+var chessCard = document.getElementById("chess-card");
+var cubeCard = document.getElementById("cube-card");
+
+// volleyball animation
+volleyballCard.addEventListener("mouseover", function() {
+    var volleyballImg = document.getElementById("volleyball-img");
+    volleyballImg.style.opacity = 1;
+});
+
+volleyballCard.addEventListener("mouseout", function() {
+    var volleyballImg = document.getElementById("volleyball-img");
+    volleyballImg.style.opacity = 0;
+});
+
+// chess
+chessCard.addEventListener("mouseover", function() {
+    var chessImg = document.getElementById("chess-img");
+    chessImg.style.opacity = 1;
+});
+
+chessCard.addEventListener("mouseout", function() {
+    var chessImg = document.getElementById("chess-img");
+    chessImg.style.opacity = 0;
+});
+
+cubeCard.addEventListener("mouseover", function() {
+    var cubeImg = document.getElementById("cube-img");
+    cubeImg.style.opacity = 1;
+});
+
+cubeCard.addEventListener("mouseout", function() {
+    var cubeImg = document.getElementById("cube-img");
+    cubeImg.style.opacity = 0;
+});
