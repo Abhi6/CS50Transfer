e7fcfeadd03a51908597a42666b04adf77584176
Abhi6 <abhinav.mara@gmail.com>
2022-12-11 03:25:30 -0500

    finished career page

--- psets/pset8/homepage/pages/scriptsDir/career.js
+++ psets/pset8/homepage/pages/scriptsDir/career.js
@@ -1,1 +1,8 @@
-console.log('career testing')
+var circle = document.querySelector(".circle");
+
+document.addEventListener("mousemove", function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+});
